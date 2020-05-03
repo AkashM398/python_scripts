72408d9080b27435978fb5e8832ca3ecc68af41d
Akash M <akashm1999@gmail.com>
2020-05-03 20:48:14 +0530

    added object detection

---
+++ object_detect.py
@@ -0,0 +1,13 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detect = ObjectDetection()
+detect.setModelTypeAsRetinaNet()
+detect.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detect.loadModel()
+detections = detect.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
